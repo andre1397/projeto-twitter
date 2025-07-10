ed69e2c387bcee7dfcab69ea3496620197e1d6a1
André Luiz dos Santos <andreluiz1793@gmail.com>
2025-07-10 13:35:55 -0300

    Update GlobalExceptionHandler.java

--- Twitter-project/src/main/java/tech/buildrun/springsecurity/exception/GlobalExceptionHandler.java
+++ Twitter-project/src/main/java/tech/buildrun/springsecurity/exception/GlobalExceptionHandler.java
@@ -9,12 +9,12 @@ import org.springframework.web.server.ResponseStatusException;
 
 import tech.buildrun.springsecurity.dto.ErrorResponse;
 
-@RestControllerAdvice//Informa que essa classe deve interceptar as exceptions lançadas pelos controllers da aplicação, permitindo que você trate essas exceções de forma centralizada. Pra funcionar, essas exceptions não devem ser tratadas em try/catch das suas classes e em nenhum outro lugar, pois o @RestControllerAdvice vai interceptar essas exceptions e tratá-las aqui, retornando uma resposta adequada para o cliente. Se você tratar as exceptions em outros lugares, o @RestControllerAdvice não será acionado.
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(BadCredentialsException.class)//Informa qual tipo de exception esse método vai tratar. Nesse caso, ele vai tratar as exceptions do tipo BadCredentialsException, que são lançadas quando as credenciais do usuário (como nome de usuário ou senha) são inválidas. Qualquer exception desse tipo que estourar no sistema será capturada por esse método, e a resposta adequada será retornada para o cliente.
+    @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<ErrorResponse> handleBadCredentialsException(BadCredentialsException ex) {
-        ErrorResponse errorResponse = new ErrorResponse("401 UNAUTHORIZED", ex.getMessage());//ex.getMessage é pra pegar a mensagem personalizada que está dentro da exception criada na classe, nesse caso aqui é no UserService, a mensagem está sendo setada lá.
+        ErrorResponse errorResponse = new ErrorResponse("401 UNAUTHORIZED", ex.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
     }
 
