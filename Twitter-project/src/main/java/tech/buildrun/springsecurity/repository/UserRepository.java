ed6e8249902805e677425e407005d1550d176c81
André Luiz dos Santos <andreluiz1793@gmail.com>
2025-07-10 13:36:18 -0300

    Update UserRepository.java

--- Twitter-project/src/main/java/tech/buildrun/springsecurity/repository/UserRepository.java
+++ Twitter-project/src/main/java/tech/buildrun/springsecurity/repository/UserRepository.java
@@ -11,12 +11,5 @@ import tech.buildrun.springsecurity.entities.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-    // Este repositório é usado para acessar os dados da tabela de usuários no banco de dados.
-    // Ele estende JpaRepository, que fornece métodos para operações CRUD (Create, Read, Update, Delete).
-    // O tipo de entidade é Role e o tipo de ID é Long.
-    
-    // Aqui você pode adicionar métodos personalizados se necessário, por exemplo:
-    // Optional<Role> findByRoleName(String roleName);
-
-    Optional<User> findByUsername(String username); // Método para encontrar um usuário pelo nome de usuário. Isso é útil para autenticação e verificação de existência de usuários no sistema.
+    Optional<User> findByUsername(String username);
 }
