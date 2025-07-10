c900db1aa47c34040d986de2fd89d9a5c0cc6759
André Luiz dos Santos <andreluiz1793@gmail.com>
2025-07-10 13:34:33 -0300

    Update Role.java

--- Twitter-project/src/main/java/tech/buildrun/springsecurity/entities/Role.java
+++ Twitter-project/src/main/java/tech/buildrun/springsecurity/entities/Role.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_roles")//Roles são os perfis de acesso no sistema, como por exemplo, ADMIN, USER, etc. Cada usuário pode ter um ou mais roles associados a ele. A tabela de roles é usada para armazenar esses perfis de acesso.
+@Table(name = "tb_roles")
 public class Role {
 
     @Id
@@ -16,7 +16,7 @@ public class Role {
     @Column(name = "role_id")
     private Long roleId;
 
-    private String roleName;//Como é o hibernate que está criando a tabela,e não está sendo definido um name pro campo, o nome da coluna no banco de dados será role_name, pois o hibernate converte automaticamente os nomes dos campos para snake_case.
+    private String roleName;
 
     public Role() {
     }
@@ -42,8 +42,6 @@ public class Role {
     }
 
     public enum Values{
-        // Valores predefinidos para as roles, como ADMIN e BASIC. Esses valores podem ser usados para criar roles padrão no sistema.
-        // O valor associado a cada role é um long que representa o ID da role no banco
         ADMIN(1L), BASIC(2L); // No banco ele cria a coluna com o nome das roles com letras minusculas e separadas por underline, ou seja, admin e basic
 
         long roleId;
